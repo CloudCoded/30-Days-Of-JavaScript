5fb00cbf24885b7c1bf5c2ac6dd995c7f49ac16b
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-01 08:06:51 -0500

    DAY 5

--- Day_5/Exercise_3.js
+++ Day_5/Exercise_3.js
@@ -1,3 +1,22 @@
 /*
-Question 1: 
+Question 1: The following is an array of 10 students ages:
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+Sort the array and find the min and max age
+Find the median age(one middle item or two middle items divided by two)
+Find the average age(all items divided by number of items)
+Find the range of the ages(max minus min)
+Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
 */
+
+//Answer => sort array and find the min and max age
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+let sortAges = ages.sort(); //sort the array
+let minAge = Math.min.apply(Math, sortAges); //Math.min method uses the apply method to print the min age
+let maxAge = Math.max.apply(Math, sortAges); //Math.min method uses the apply method to print the min age
+
+console.log(sortAges); //output sorted array
+console.log(minAge); //min age 19
+console.log(maxAge); //max age 26
--- Day_5/README.md
+++ Day_5/README.md
@@ -3,3 +3,5 @@ What I Learned On 27th Feb 2023
 i. Arrays
 ii. Methods to manipulate array
 iii. Arrays of arrays
+
+Why working on the task I learned about Math.min.apply(Math, arr) or Math.min(...arr) // Math.max.apply(Math, arr) or Math.max(...arr) to print the min and max number in an array respectively.
