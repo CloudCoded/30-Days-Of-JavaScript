9c20b2b648a241c71cf7e4db9838787822c6b57a
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-01 12:10:51 -0500

    DAY 5

--- Day_5/Exercise_3.js
+++ Day_5/Exercise_3.js
@@ -7,7 +7,7 @@ Sort the array and find the min and max age
 Find the median age(one middle item or two middle items divided by two)
 Find the average age(all items divided by number of items)
 Find the range of the ages(max minus min)
-Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+Compare the value of (min - average) and (max - average), use abs() method 
 */
 
 //Answer => sort array and find the min and max age
@@ -27,14 +27,14 @@ ages.sort(function(a, b) {
   return a - b;
 });
 
-const length = ages.length;
-const middle = Math.floor(length / 2);
+const length = ages.length; //gives the length of the array
+const middle = Math.floor(length / 2); //divides the array into two and return the absolute num
 
 if (length % 2 === 0) {
-  const median = (ages[middle - 1] + ages[middle]) / 2;
+  const median = (ages[middle - 1] + ages[middle]) / 2; //add the last num in the first arr and the first num in the second arr and divide the numbers by 2
   console.log(median); // Output: 24
 } else {
-  const median = ages[middle];
+  const median = ages[middle]; //but if its jus a num in the middle, print it
   console.log(median); // Output: 24
 }
 
@@ -53,3 +53,10 @@ const comValueMax = Math.abs(Math.floor(26- 22.8));
 
 console.log(comValueMin);
 console.log(comValueMax);
+
+//1.Slice the first ten countries from the countries array
+
+//Check main.js => countries.js
+
+// Find the middle country(ies) in the countries array
+//Check main.js => countries.js
--- Day_5/main.js/countries.js
+++ Day_5/main.js/countries.js
@@ -19,3 +19,19 @@ const countries = [
   } else{
     console.log('ETHOPIA') // print ETHIOPIA if it exist in arr
   }
+
+  //Exercise 3
+  // 1.Slice the first ten countries from the countries array
+  console.log(countries.slice(0, 9))
+
+  // Find the middle country(ies) in the countries array
+  const length = countries.length; //gives the length of the array
+  const middle = Math.floor(length / 2) //divides the array into two and return the absolute num 
+
+  if(length % 2 === 0){
+    const middleCountry = (countries[middle - 1] + countries[middle]) 
+    console.log(middleCountry.toString(' ')); //if the lengths divided doesn't have a reminder, it will add the last country in the first array and first country in the second array together, turn it into string and print the two countries
+  } else{
+    const middleCountry = countries[middle]
+    console.log(middleCountry); // else if there's a reminder then that will be the middle country and will print it
+  }
