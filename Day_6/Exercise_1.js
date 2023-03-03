acc8cec8a7b1782de04e01bd2ae650571746d4c7
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-03 12:09:16 -0500

    DAY 6

--- Day_6/Exercise_1.js
+++ Day_6/Exercise_1.js
@@ -81,11 +81,22 @@ for(let i = 0; i <= 10; ++i){
 Question 7: Use for loop to iterate from 0 to 100 and print only even numbers
 */
 
-
+for(let i = 0; i <= 100; ++i){
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
 
 /*
-Question 4: 
+Question 8: Use for loop to iterate from 0 to 100 and print only odd numbers
 */
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
+
 /*
 Question 4: 
 */
