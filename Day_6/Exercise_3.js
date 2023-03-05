69e5612b471c2e37aee7c98bb53a08605b9bb556
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-05 12:24:23 -0500

    DAY 6

--- Day_6/Exercise_3.js
+++ Day_6/Exercise_3.js
@@ -20,3 +20,23 @@ Question 3: Sort the webTechs array and mernStack array
 //Answer
 const sortWebTechs = webTechs.sort();
 const sortMernStack = mernStack.sort();
+
+/*
+Question 4: Extract all the countries contain the word 'land' from the countries array and print it as array
+*/
+
+//Answer
+
+let countriesWithLand = [];
+
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].toLowerCase().includes('land')) {
+    countriesWithLand.push(countries[i]);
+  }
+}
+
+if (countriesWithLand.length > 0) {
+  console.log(countriesWithLand);
+} else {
+  console.log('All these countries are without land');
+}
