546c66cb9b571e7dc437461ea0bb9b21982c6524
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-11 10:05:16 -0500

    code update

--- Day_5/Exercise_1.js
+++ Day_5/Exercise_1.js
@@ -47,7 +47,7 @@ const personalInfo = [
     {country: 'Nigeria'},
     NaN
 ]
-console.log(personalInfo);
+console.log(personalInfo.length);
 
 /*
 Question 6: Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
