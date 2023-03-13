e4586d3331d7a7a875fe69c3b0d3e9270a709672
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-13 18:11:36 -0400

    day 7

---
+++ Day_7/Exercise_1.js
@@ -0,0 +1,56 @@
+/*
+Question 1: Declare a function fullName and it print out your full name.
+*/
+
+//Answer
+function fullName (){
+    let firstName = 'Ali '
+    let lastName = 'Baba'
+    let name = firstName.concat(lastName);
+
+    console.log(name);
+}
+
+fullName ();
+
+/*
+Question 2: Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+*/
+
+//Answer
+function fullName (firstName, lastName){
+    let name = `${firstName}  ${lastName}`
+    console.log(name);
+}
+fullName('Asabeneh', 'Peter')
+
+/*
+Question 3: 
+*/
+/*
+Question 4: 
+*/
+/*
+Question 5: 
+*/
+/*
+Question 6: 
+*/
+/*
+Question 7: 
+*/
+/*
+Question 8: 
+*/
+/*
+Question 9: 
+*/
+/*
+Question 10: 
+*/
+/*
+Question 11: 
+*/
+/*
+Question 12: 
+*/
