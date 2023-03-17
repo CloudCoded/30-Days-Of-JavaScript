a643a9a81b7c5d850f40abb19ad218de2832c41c
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-17 06:57:26 -0400

    day 7

--- Day_7/Exercise_2.js
+++ Day_7/Exercise_2.js
@@ -15,8 +15,22 @@ function printArray(arr) {
 printArray([1, 2, 3, 4]);
 
 /*
-Question 4: 
+Question 4: Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
 */
+
+//Answer
+const showDateTime = () => {
+    const now = new Date();
+    const day = now.getDay();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+
+    console.log(`${day}/${month}/${year} ${hour}:${minutes}`);
+}
+showDateTime()
+
 /*
 Question 5: 
 */
