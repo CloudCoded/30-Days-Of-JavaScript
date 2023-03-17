63d6039f4361353e6feeea6b0e146676d83064ec
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-17 06:37:54 -0400

    day 7

---
+++ Day_7/Exercise_2.js
@@ -0,0 +1,64 @@
+/*
+Question 1: 
+*/
+/*
+Question 2: 
+*/
+/*
+Question 3: Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+*/
+
+//Answer
+function printArray(arr) {
+    console.log(arr)
+}
+printArray([1, 2, 3, 4]);
+
+/*
+Question 4: 
+*/
+/*
+Question 5: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
+/*
+Question 1: 
+*/
