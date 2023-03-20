634a3eb9ed3af612fded3562c5184e98bb7e7778
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-20 05:41:51 -0400

    day 7

---
+++ Day_7/Exercise_3.js
@@ -0,0 +1,60 @@
+/*
+Question 1: 
+*/
+/*
+Question 2: 
+*/
+/*
+Question 3: 
+*/
+/*
+Question 4: 
+*/
+/*
+Question 5: 
+*/
+/*
+Question 6: 
+*/
+/*
+Question 7: 
+*/
+/*
+Question 8: 
+*/
+/*
+Question 9: 
+*/
+/*
+Question 10: 
+*/
+/*
+Question 11: 
+*/
+/*
+Question 12: 
+*/
+/*
+Question 13: 
+*/
+/*
+Question 14: 
+*/
+/*
+Question 15: 
+*/
+/*
+Question 16: 
+*/
+/*
+Question 17: 
+*/
+/*
+Question 18: 
+*/
+/*
+Question 19: 
+*/
+/*
+Question 20: 
+*/
