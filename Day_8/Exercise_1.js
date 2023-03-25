94e031d3a32ac02f50487096b947f89552e788a0
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-03-25 09:24:00 -0400

    day 8

--- Day_8/Exercise_1.js
+++ Day_8/Exercise_1.js
@@ -4,3 +4,20 @@ Question 1: Create an empty object called dog
 
 //Answer
 const dog = {};
+
+/*
+Question 2: Print the the dog object on the console
+*/
+
+//Answer
+console.log(dog);
+
+/*
+Question 3: 
+*/
+/*
+Question 4: 
+*/
+/*
+Question 5: 
+*/
