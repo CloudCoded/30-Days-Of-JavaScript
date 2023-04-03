9f57f24ce68f2f0956653867dc3925ddc227dd9e
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-03 05:36:20 -0400

    day 8

--- Day_8/Exercise_3.js
+++ Day_8/Exercise_3.js
@@ -123,7 +123,50 @@ Imagine you are getting the above users collection from a MongoDB database. a. C
 b. Create a function called signIn which allows user to sign in to the application
 */
 
-//Answer
+const users = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+    ];
+
+//Answer (A)
 function signUp(user){
     let existingUser = false;
 
@@ -151,6 +194,19 @@ const newUser = {
     isLoggedIn: true
 };
 signUp(newUser);
+   
+  // (B)
+
+  function signIn(email, password) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email === email && users[i].password === password) {
+        users[i].isLoggedIn = true;
+        console.log(alert('Welcome'));
+        return;
+      }
+    }
+    console.log('Invalid email or password.');
+  }
   
 
 /*
