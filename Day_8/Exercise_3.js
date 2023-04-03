23e67aeb33077bd2fd26e458db6898d4bdc77eaf
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-03 03:34:45 -0400

    day 8

--- Day_8/Exercise_3.js
+++ Day_8/Exercise_3.js
@@ -1,6 +1,14 @@
 /*
-Question 1: 
+Question 1: Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
 */
+
+//Answer
+const personAccount = {
+    firstName: 'Alex',
+    lastName: 'Ekubo',
+    incomes: 500,
+    expenses: 200
+}
 
 /*
 Question 2: 
