01f95b4c03376ef61cbf55222348975bd357c62a
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-03 04:18:36 -0400

    day 8

--- Day_8/Exercise_3.js
+++ Day_8/Exercise_3.js
@@ -6,8 +6,40 @@ Question 1: Create an object literal called personAccount. It has firstName, las
 const personAccount = {
     firstName: 'Alex',
     lastName: 'Ekubo',
-    incomes: 500,
-    expenses: 200
+    incomes: [],
+    expenses: [],
+
+    totalIncome: function(){
+        total = 0;
+        for(let i = 0; i < this.incomes.length; i++){
+            total += this.incomes[i];
+        }
+        return total;
+    },
+
+    totalExpenses: function(){
+        total = 0;
+        for(let i = 0; i < this.expenses.length; i++){
+            total += this.expenses[i];
+        }
+        return total;
+    },
+
+    accountInfo: function(){
+        return `Name ${this.firstName} ${this.lastName}\nTotal Income: ${this.totalIncome()}\nTotal Expenses: ${this.totalExpenses()}\nAccount Balance: ${this.accountBalance()}`;
+    },
+
+    addIncome: function(description, amount){
+        this.incomes.push(amount);
+    },
+
+    addExpenses: function(description, amount){
+        this.expenses.push(amount)
+    },
+
+    accountBalance: function(){
+        return this.totalIncome() - this.totalExpenses();
+    }
 }
 
 /*
