3b471e4db7d375b2faf4d60fccbbe427215709b1
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-06 18:36:44 -0400

    day 9

--- Day_9/Exercise_1.js
+++ Day_9/Exercise_1.js
@@ -9,4 +9,10 @@ Question 1: Explain the difference between forEach, map, filter, and reduce.
 const array1 = ['a', 'b', 'c'];
 array1.forEach(element => console.log(element)) // a b c
 
+//map
+'The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.'
 
+//Example
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const toUpperCase = names.map((name) => name.toUpperCase());
+console.log(toUpperCase) //['ASABENEH', 'MATHIAS', 'ELIAS', 'BROOK']
