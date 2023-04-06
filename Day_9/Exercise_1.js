ae13a386dd0cc8ffa199d612f93e0a214db959ae
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-06 18:21:40 -0400

    day 9

---
+++ Day_9/Exercise_1.js
@@ -0,0 +1,12 @@
+/*
+Question 1: Explain the difference between forEach, map, filter, and reduce.
+*/
+
+//Answer forEach
+'The forEach() method executes a provided function once for each array element. It iterates an array elements and can only be use with array'
+
+//Example
+const array1 = ['a', 'b', 'c'];
+array1.forEach(element => console.log(element)) // a b c
+
+
