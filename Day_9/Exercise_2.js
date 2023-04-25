fa367d72601c71a4728b57a073b86872bcf34565
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-25 04:03:04 -0400

    day 9

--- Day_9/Exercise_2.js
+++ Day_9/Exercise_2.js
@@ -22,6 +22,7 @@ Question 2: Find the sum of price of products using only reduce reduce(callback)
 */
 
 //Answer
+const sumPrice = products.reduce((acc, currVal) => acc + currVal);
 
 /*
 Question 3: Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
