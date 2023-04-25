3edbe7e62038094f282d5fc273a4bbbbb9a5998d
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-25 03:59:56 -0400

    day 9

--- Day_9/Exercise_2.js
+++ Day_9/Exercise_2.js
@@ -3,6 +3,19 @@ Question 1: Find the total price of products by chaining two or more array itera
 */
 
 //Answer
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+  
+const totalPrice = products.filter(product => typeof product.price === 'number').map(product => product.price).reduce((accumulator, currentValue) => accumulator + currentValue)
+  
+console.log(totalPrice) // Output: 27 (3 + 6 + 8 + 10)
+  
 
 /*
 Question 2: Find the sum of price of products using only reduce reduce(callback))
