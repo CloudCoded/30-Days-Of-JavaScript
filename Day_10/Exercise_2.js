5d938f34bd1ec97e0064890f9d48443a73a42a77
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-06 15:31:28 -0400

    day 10

--- Day_10/Exercise_2.js
+++ Day_10/Exercise_2.js
@@ -1,6 +1,18 @@
 /*
 Question 1: Find a union b
 */
+
+//Answer
+let a = [1, 2, 3, 4, 5]
+let b =[3, 4, 5, 6]
+let c = [...a, ...b] //spread method
+
+let aSet = new Set(a)
+let bSet = new Set(b)
+let cSet = new Set(c)
+
+console.log(cSet);
+
 /*
 Question 2: 
 */
