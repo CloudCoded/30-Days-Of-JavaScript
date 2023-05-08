a1785b821aae9b11f3868d15493894c25548f283
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-08 09:47:57 -0400

    day 10

--- Day_10/Exercise_3.js
+++ Day_10/Exercise_3.js
@@ -4,3 +4,12 @@ Question 1: How many languages are there in the countries object file.
 
 //Answer
 import countries from '/countries.js'
+
+const allLang = () => {
+    let count = 0;
+    for(let i = 0; i < countries.length; i++){
+        count++;
+    }
+    console.log(count);
+}
+allLang();
