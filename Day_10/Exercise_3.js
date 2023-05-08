30a0d6de5a44624985857bcbe6a4f7f3d80924ab
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-08 11:23:13 -0400

    day 10

--- Day_10/Exercise_3.js
+++ Day_10/Exercise_3.js
@@ -43,9 +43,9 @@ Question 2: *** Use the countries data to find the 10 most spoken languages:
 */
 
 //Answer
-const mostSpokenLanguages = (arr, num) => {
-    const lanCount = new Map();
-    arr.forEach(country => {
+const mostSpokenLanguages = (arr, num) => { //This line declares a function called mostSpokenLanguages that takes two parameters: an array arr and a number num.
+    const lanCount = new Map(); //This line initializes a new Map object called lanCount, which will be used to count the number of occurrences of each language in the arr array.
+    arr.forEach(country => { 
         country.language.forEach(lang => {
             if(!lanCount.has(lang)){
                 lanCount.set(lang, 1);
@@ -56,9 +56,20 @@ const mostSpokenLanguages = (arr, num) => {
         });
     });
 
-    const sorted = [...lanCount.entries()].sort((a, b) => b[1] - a[1]);
-    console.log(sorted.slice(0, num));
-    return sorted.slice(0, num);
+    /*
+    This code uses nested forEach loops to iterate through each country object in the arr array, and then through each language in the language array of that country. It then checks whether the lanCount Map already has an entry for that language. If not, it sets the count for that language to 1. If it does, it increments the count for that language.
+    */
+
+    const sorted = [...lanCount.entries()].sort((a, b) => b[1] - a[1]); 
+
+    /*
+    This line converts the lanCount Map into an array of [key, value] pairs using the entries() method of the Map object, spreads it into a new array using the spread syntax (...), and then sorts the array in descending order based on the second element (value) of each pair.
+    */
+
+    console.log(sorted.slice(0, num)); // This line logs the first num elements of the sorted array to the console, which represents the num most frequently spoken languages.
+    return sorted.slice(0, num); //This line returns the first num elements of the sorted array as the output of the mostSpokenLanguages function.
+
+
 };
 
 mostSpokenLanguages(countries, 10);
