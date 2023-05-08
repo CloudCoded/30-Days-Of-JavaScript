109032481a6256d5a9c98087eef9f843fa75f9fe
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-08 09:50:25 -0400

    day 10

--- Day_10/Exercise_3.js
+++ Day_10/Exercise_3.js
@@ -13,3 +13,31 @@ const allLang = () => {
     console.log(count);
 }
 allLang();
+
+/*
+Question 2: *** Use the countries data to find the 10 most spoken languages:
+
+   // Your output should look like this
+   console.log(mostSpokenLanguages(countries, 10))
+   [
+     { English: 91 },
+     { French: 45 },
+     { Arabic: 25 },
+     { Spanish: 24 },
+     { Russian: 9 },
+     { Portuguese: 9 },
+     { Dutch: 8 },
+     { German: 7 },
+     { Chinese: 5 },
+     { Swahili: 4 },
+     { Serbian: 4 }
+   ]
+
+  // Your output should look like this
+  console.log(mostSpokenLanguages(countries, 3))
+  [
+  {English:91},
+  {French:45},
+  {Arabic:25}
+  ]
+*/
