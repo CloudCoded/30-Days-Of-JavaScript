a1575aee8c479917f6184c74f40206253e72bb8f
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-11 14:33:07 -0400

    day 11

--- Day_11/Exercise_1.js
+++ Day_11/Exercise_1.js
@@ -19,5 +19,56 @@ let [fin, est, sw, den, nor] = countries //Destructing
 console.log(fin, est, sw, den, nor);
 
 /*
-Question 3: 
+Question 3: Destructure the rectangle object by its properties or keys.
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60
+}
+const users = [
+{
+  name:'Brook',
+  scores:75,
+  skills:['HTM', 'CSS', 'JS'],
+  age:16
+},
+{
+  name:'Alex',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'David',
+  scores:75,
+  skills:['HTM', 'CSS'],
+  age:22
+},
+{
+  name:'John',
+  scores:85,
+  skills:['HTML'],
+  age:25
+},
+{
+  name:'Sara',
+  scores:95,
+  skills:['HTM', 'CSS', 'JS'],
+  age: 26
+},
+{
+  name:'Martha',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'Thomas',
+  scores:90,
+  skills:['HTM', 'CSS', 'JS'],
+  age:20
+}
+]
 */
