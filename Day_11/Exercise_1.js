36661e7a1dbb88b662d8340398812ec7c849b498
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-11 14:28:24 -0400

    day 11

---
+++ Day_11/Exercise_1.js
@@ -0,0 +1,16 @@
+/*
+Question 1: Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+*/
+
+//Answer
+const constants = [2.72, 3.14, 9.81, 37, 100]
+
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants //Destructing
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);
+
+/*
+Question 2: 
+*/
+/*
+Question 3: 
+*/
