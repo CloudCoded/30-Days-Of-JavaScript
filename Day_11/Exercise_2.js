ee00861172d511373df3412703a027b44ef81cdc
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-12 09:53:35 -0400

    day 11

--- Day_11/Exercise_2.js
+++ Day_11/Exercise_2.js
@@ -1,6 +1,57 @@
 /*
 Question 1: Iterate through the users array and get all the keys of the object using destructuring
 */
+
+//Answer
+const users = [
+    {
+      name:'Brook',
+      scores:75,
+      skills:['HTM', 'CSS', 'JS'],
+      age:16
+    },
+    {
+      name:'Alex',
+      scores:80,
+      skills:['HTM', 'CSS', 'JS'],
+      age:18
+    },
+    {
+      name:'David',
+      scores:75,
+      skills:['HTM', 'CSS'],
+      age:22
+    },
+    {
+      name:'John',
+      scores:85,
+      skills:['HTML'],
+      age:25
+    },
+    {
+      name:'Sara',
+      scores:95,
+      skills:['HTM', 'CSS', 'JS'],
+      age: 26
+    },
+    {
+      name:'Martha',
+      scores:80,
+      skills:['HTM', 'CSS', 'JS'],
+      age:18
+    },
+    {
+      name:'Thomas',
+      scores:90,
+      skills:['HTM', 'CSS', 'JS'],
+      age:20
+    }
+    ]
+
+    for(const{name, scores, skills, age} of users){
+        console.log(name, scores, skills, age);
+    }
+
 /*
 Question 2: 
 */
