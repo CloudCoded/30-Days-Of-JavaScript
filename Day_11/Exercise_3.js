55cb4552ecac5df4dde0422c9b7e8ee4ebb4f921
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-13 13:58:13 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -62,3 +62,13 @@ const convertArrayToObject = (array) => {
 const studentsObject = convertArrayToObject(students);
 console.log(studentsObject);
 
+/*
+Question 4: Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+
+Add Bootstrap with level 8 to the front end skill sets
+Add Express with level 9 to the back end skill sets
+Add SQL with level 8 to the data base skill sets
+Add SQL without level to the data science skill sets
+*/
+
+//Answer
