5a441ad275a4889e42ac9ffd42b1cd4c0d2a4a08
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-13 17:23:59 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -72,3 +72,31 @@ Add SQL without level to the data science skill sets
 */
 
 //Answer
+const studentss = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
+
+  const newStudent = {...studentss}
+   newStudent.skills.frontEnd.push({ skill: 'Bootstrap', level: 8})
+   newStudent.skills.backEnd.push({ skill: 'Express', level: 9})
+   newStudent.skills.dataBase.push({ skill: 'SQL', level: 8})
+   newStudent.skills.dataScience.push({ skill: 'SQL'})
+
+   console.log(newStudent);
