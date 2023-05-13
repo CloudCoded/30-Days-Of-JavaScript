48cdbf1e100adeb04f04135bd1cb627028bdd872
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-13 12:24:07 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -8,3 +8,9 @@ require('./countries_data')
 for(const {name, capital, population, languages} of countries){
     console.log({name, capital, population, languages})
 }
+
+/*
+Question 2: A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+*/
