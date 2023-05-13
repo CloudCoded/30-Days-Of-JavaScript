4e062811f0576f8e62e523cd3c39c8a305bd2a72
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-13 12:22:08 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -1,3 +1,10 @@
 /*
 Question 1: Destructure the countries object print name, capital, population and languages of all countries
 */
+
+//Answer
+require('./countries_data')
+
+for(const {name, capital, population, languages} of countries){
+    console.log({name, capital, population, languages})
+}
