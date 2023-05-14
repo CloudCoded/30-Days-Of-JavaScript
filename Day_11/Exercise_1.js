0c016288e3196ea40a386cf1c99fc36fd17b4ee4
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-14 15:17:53 -0400

    day 11

--- Day_11/Exercise_1.js
+++ Day_11/Exercise_1.js
@@ -3,7 +3,7 @@ Question 1: Destructure and assign the elements of constants array to e, pi, gra
 */
 
 //Answer
-const constants = [2.72, 3.14, 9.81, 37, 100]
+const constants = [2.72, 3.14, 9.81, 37, 100];
 
 let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants //Destructing
 console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);
