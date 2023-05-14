727c6539f7e5424b89d62d005da286919ba5f4fa
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-14 15:11:42 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -16,7 +16,7 @@ const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
 */
 
 //Answer
-const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
 const [name, skills, [, ,jsScore, reactScore]] = student
 console.log(name, skills, jsScore, reactScore);
 
