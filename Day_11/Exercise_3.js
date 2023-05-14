77679451a4fbab9e6fcd5bd8cbe577e21e2139f2
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-14 15:14:42 -0400

    day 11

--- Day_11/Exercise_3.js
+++ Day_11/Exercise_3.js
@@ -95,7 +95,7 @@ const studentss = {
 
   const newStudent = {...studentss};
    newStudent.skills.frontEnd.push({ skill: 'Bootstrap', level: 8});
-   newStudent.skills.backEnd.push({ skill: 'Express', level: 9})
+   newStudent.skills.backEnd.push({ skill: 'Express', level: 9});
    newStudent.skills.dataBase.push({ skill: 'SQL', level: 8})
    newStudent.skills.dataScience.push({ skill: 'SQL'})
 
