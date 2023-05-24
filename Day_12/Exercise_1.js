d661e4838de035db2d673ea1f9e559ef48b9ee87
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-24 10:18:36 -1200

    day 12

--- Day_12/Exercise_1.js
+++ Day_12/Exercise_1.js
@@ -1,6 +1,10 @@
 /*
-Question 1: 
+Question 1: Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
 */
+
+//Answer
+
+
 /*
 Question 2:
 */
