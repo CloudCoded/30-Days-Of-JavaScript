cc8036916f999bfcc9b0743da2cc9f4165bc4b9e
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-24 13:14:27 -1200

    day 12

--- Day_12/Exercise_1.js
+++ Day_12/Exercise_1.js
@@ -3,7 +3,14 @@ Question 1: Calculate the total annual income of the person from the following t
 */
 
 //Answer
+const sentence = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
+const extractNums = sentence.match(/\d+/g)
+const monthSalary = parseInt(num[0])
+const bonus = parseInt(num[1])
+const onlineMonie = parseInt(num[2])
 
+const annualIncome = (monthSalary * 12) + bonus + (onlineMonie * 12)
+console.log(annualIncome);
 
 /*
 Question 2:
