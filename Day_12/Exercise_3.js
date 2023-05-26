061884f96df8153e9e9f82e9a2a80fa771f71a94
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-26 11:47:59 -1200

    day 12

--- Day_12/Exercise_3.js
+++ Day_12/Exercise_3.js
@@ -1,3 +1,8 @@
 /*
 Question 1: Writ a function which cleans text. Clean the following text. After cleaning, count three most frequent words in the string.
 */
+
+//Answer
+sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+const cleanText = sentence.replace(/[^\s\w]/gi, '')
+console.log(cleanText)
