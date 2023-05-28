6e93f091bc775d8d742ee259f00511ec84066062
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-28 07:34:51 -1200

    day 13

--- Day_13/Exercise_2.js
+++ Day_13/Exercise_2.js
@@ -4,3 +4,9 @@ Question 1: 10 > 2 * 10 use console.assert()
 
 //Answer
 console.assert(10 > 2 * 10, "Assertion failed: 10 is not greater than 2 multiplied by 10");
+
+/*
+Question 2: Write a warning message using console.warn()
+*/
+
+console.warn('Tailwind is not inline styling');
