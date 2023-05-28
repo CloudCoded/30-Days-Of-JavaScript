509bcbffac273653588c1a77cca18d8d216434c7
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-27 16:49:06 -1200

    day 13

---
+++ Day_13/Exercise_1.js
@@ -0,0 +1,11 @@
+/*
+Question 1: Display the countries array as a table
+*/
+
+//Answer
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+console.table(countries);
