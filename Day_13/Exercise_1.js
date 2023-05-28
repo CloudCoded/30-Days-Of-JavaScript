5ce6baf544ef3792c5fe416fba480d4d352cc810
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-05-27 17:14:53 -1200

    day 13

--- Day_13/Exercise_1.js
+++ Day_13/Exercise_1.js
@@ -17,3 +17,56 @@ Question 2: Display the countries object as a table
 //Answer
 import './countries_data'
 console.table(countries);
+
+/*
+Question 3: Use console.group() to group logs
+*/
+
+//Answer
+const names = ['Asabeneh', 'Brook', 'David', 'John']
+const user = {
+  name: 'Asabeneh',
+  title: 'Programmer',
+  country: 'Finland',
+  city: 'Helsinki',
+  age: 250
+}
+const users = [
+  {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+  },
+  {
+    name: 'Eyob',
+    title: 'Teacher',
+    country: 'Sweden',
+    city: 'London',
+    age: 25
+  },
+  {
+    name: 'Asab',
+    title: 'Instructor',
+    country: 'Norway',
+    city: 'Oslo',
+    age: 22
+  },
+  {
+    name: 'Matias',
+    title: 'Developer',
+    country: 'Denmark',
+    city: 'Copenhagen',
+    age: 28
+  }
+]
+
+console.group('Names')
+console.log(names)
+console.groupEnd()
+
+console.group('Users')
+console.log(user)
+console.log(users)
+console.groupEnd()
