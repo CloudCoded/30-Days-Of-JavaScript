a754116f53fccbe8bb0e3336ea93d2cd717e08bb
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-06-01 05:54:37 -1200

    day 15

--- Day_15/Exercise_1.js
+++ Day_15/Exercise_1.js
@@ -1,3 +1,17 @@
 /*
 Question 1: Create an Animal class. The class will have name, age, color, legs properties and create different methods
 */
+
+//Answer
+class Animal {
+    constructor(name, age, color, legs){
+        console.log(this)
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.legs = legs;
+    }
+}
+
+const dog = new Animal('Ghost', 6, 'Black', 'transparent');
+console.log(dog);
