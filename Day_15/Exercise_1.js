dff4bf58a86640c9670816bf4cd4e27f0f4e637d
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-06-01 07:58:05 -1200

    day 15

--- Day_15/Exercise_1.js
+++ Day_15/Exercise_1.js
@@ -13,9 +13,41 @@ class Animal {
     }
 }
 
-const dog = new Animal('Ghost', 6, 'Black', 'transparent');
-console.log(dog);
+const newAnimal = new Animal('Ghost', 6, 'Black', 'transparent');
+console.log(newAnimal);
 
 /*
 Question 2: Create a Dog and Cat child class from the Animal Class.
 */
+
+//Answer
+class Animal {
+    constructor(name, age, color, legs){
+        console.log(this)
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.legs = legs;
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, color, legs){
+        super(name, age, color, legs)
+        this.pet1 = pet1
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, age, color, legs){
+        super(name, age, color, legs)
+        this.pet2 = pet2;
+    }
+}
+
+const myDog = new Dog('Ghost', 4, 'black', 4, 'German Shepherd');
+console.log(myDog.name)
+console.log(myDog.age)
+console.log(myDog.color)
+console.log(myDog.legs)
+console.log(myDog.pet1)
