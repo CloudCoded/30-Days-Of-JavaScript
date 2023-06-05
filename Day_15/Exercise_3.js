85509c6f3191d4ab64c3ddf9ca83c5cd4d871a56
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-06-05 06:16:37 -1200

    day 15

--- Day_15/Exercise_3.js
+++ Day_15/Exercise_3.js
@@ -1,5 +1,5 @@
 /*
-Question 1: 
+Question 1: Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class
 */
 
 //Answer
