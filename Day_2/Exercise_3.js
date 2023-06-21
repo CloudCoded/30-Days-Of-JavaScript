8f6bcf811b7d5db7a9e77f49d9cd7e7b1d192ada
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-06-21 10:17:09 -1100

    revision

--- Day_2/Exercise_3.js
+++ Day_2/Exercise_3.js
@@ -17,7 +17,7 @@ Quesrtion 3: Clean the following text and find the most frequent word (hint, use
     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 */
 
-const cleared = sentence.replace(/[^\s\w]/gi, '') // remove non-alphanumeric characters
+const cleared = sentence.replace(/[^\s\w]/gi, '') // remove non-alphanumeric characters i.e special chars, whitespaces
 console.log(cleared)
 
 
