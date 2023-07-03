909fe8f284efb626013f14c314d7f58f57b1023d
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-07-03 05:16:13 -1100

    JSON

---
+++ Day_16/Exercise_1.js
@@ -0,0 +1,31 @@
+/*
+Question 1: Change skills array to JSON using JSON.stringify()
+*/
+
+import skills from "./utils"
+const txt = JSON.stringify(skills);
+console.log(txt)
+
+/*
+Question 2: Stringify the age variable
+*/
+
+import age from "./utils"
+const stringifyAge = JSON.stringify(age);
+console.log(stringifyAge)
+
+/*
+Question 3: Stringify the isMarried variable
+*/
+
+import isMarried from "./utils"
+const toStringify = JSON.stringify(isMarried);
+console.log(toStringify)
+
+/*
+Question 4: Stringify the student object
+*/
+
+import student from "./utils"
+const stringify = JSON.stringify(student);
+console.log(stringify)
