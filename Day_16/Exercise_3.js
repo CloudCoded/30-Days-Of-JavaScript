b0bd752f2f09fb1d0c589903fe08a72a41a33ab4
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-07-04 18:58:32 +0100

    JSON

--- Day_16/Exercise_3.js
+++ Day_16/Exercise_3.js
@@ -5,3 +5,8 @@ Question 1: Parse the txt JSON to object.
 import txt from "./utils"
 const stringToObj = JSON.parse(txt)
 console.log(stringToObj);
+
+/*
+Question 2: Find the user who has many skills from the variable stored in txt.
+*/
+
