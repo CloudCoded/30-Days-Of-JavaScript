0d5b2d423eabc6447c70695db0d011cf9a1b9e4c
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-06 10:32:23 +0100

    day 18

---
+++ Day_18/Exercise_1.js
@@ -0,0 +1,19 @@
+/*
+Question 1: Read the countries API using fetch and print the name of country, capital, languages, population and area.
+*/
+
+const countriesAPI = 'https://restcountries.com/v2/all'
+
+fetch (countriesAPI).then(response => response.json())
+.then(data =>{
+    data.forEach((country) =>{
+    console.log('Country:', country.name)
+    console.log('Capital:', country.capital)
+    console.log('Language:', country.languages.map((lang) => lang.name).join(', '))
+    console.log('Population:', country.population)
+    console.log('Area:', country.area)
+    }) 
+})
+.catch((error) => {
+    console.log('Error:', error)
+});
