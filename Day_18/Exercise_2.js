06fc6642024735d3456889db6eb74f7044e456f3
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-06 10:44:55 +0100

    day 18

--- Day_18/Exercise_2.js
+++ Day_18/Exercise_2.js
@@ -2,3 +2,13 @@
 Question 2: Print out all the cat names in to catNames variable.
 */
 
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+fetch (catsAPI).then(response => response.json())
+.then(data => {
+    data.forEach((cat) => {
+        console.log(`Cat Name is ${cat.name}`)
+    })
+})
+.catch((error) => {
+    // console.log('Error:', error)
+})
