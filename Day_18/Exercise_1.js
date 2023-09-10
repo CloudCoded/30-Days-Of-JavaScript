9c794bff62c95b3b94ecb8653d417cc3095c5b09
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-10 22:32:22 +0100

    day 18

--- Day_18/Exercise_1.js
+++ Day_18/Exercise_1.js
@@ -7,7 +7,7 @@ const countriesAPI = 'https://restcountries.com/v2/all';
 fetch (countriesAPI).then(response => response.json())
 .then(data =>{
     data.forEach((country) =>{
-    console.log('Country:', country.name)
+    console.log('Country:', country.name);
     console.log('Capital:', country.capital)
     console.log('Language:', country.languages.map((lang) => lang.name).join(', '))
     console.log('Population:', country.population)
