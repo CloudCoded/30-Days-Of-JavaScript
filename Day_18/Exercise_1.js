b99f191cadaf2a8fe6a0425f44d95e50c9d51ca8
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-10 22:33:19 +0100

    day 18

--- Day_18/Exercise_1.js
+++ Day_18/Exercise_1.js
@@ -10,7 +10,7 @@ fetch (countriesAPI).then(response => response.json())
     console.log('Country:', country.name);
     console.log('Capital:', country.capital);
     console.log('Language:', country.languages.map((lang) => lang.name).join(', '));
-    console.log('Population:', country.population)
+    console.log('Population:', country.population);
     console.log('Area:', country.area)
     }) 
 })
